ea736f4bb39189cead61c54a705049a6cd7bf506
Bryan Jensen <bryanawjensen@gmail.com>
2014-11-17 20:54:47 -0500

    Got api working with route of connection code

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,20 @@
-var express = require('express');
+var express = require('express'),
+    MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:route', function(req, res) {
-  console.log(req.params.route);
+router.get('/:connID', function(req, res) {
+  console.log(req.params.connID);
+  var connectionID = req.params.connID;
+  var connectionIDMatcher = new RegExp(connectionID);
 
-
-  res.send(req.params.route);
+  MongoClient.connect('mongodb://mega-group:398project2@ds053320.mongolab.com:53320/398project2', function handleResponse(err, db) {
+    console.log('Opened connection to database, reading');
+    db.collection('connections').find({ 'CourseName': { $regex: connectionID } }).toArray(function callback(err, data) {
+      console.log(data);
+      res.send(data);
+    });
+  });
 });
 
 module.exports = router;
