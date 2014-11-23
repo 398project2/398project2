e69b269d9678cc12d8c5385be37e32136da44d96
tkicks <tkicks32@gmail.com>
2014-11-23 14:30:33 -0500

    new route for courses and renamed /api to /conx

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var api = require('./routes/api');
+var connections = require('./routes/conx');
+var courses = require('./routes/courses');
 
 var app = express();
 
@@ -25,7 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/api', api);
+app.use('/conx', connections);
+app.use('/courses', courses);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/courses.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    MongoClient = require('mongodb').MongoClient;
+var router = express.Router();
+
+/* GET users listing. */
+router.route('/:id').get(function(req, res) {
+    var courseID = req.params.id;
+
+   /**/ MongoClient.connect('mongodb://mega-group:398project2@ds053320.mongolab.com:53320/398project2', function handleResponse(err, db) {
+      db.collection('courses').find({ '_id': courseID  }).toArray(function callback(err, data) {
+        if (err) { console.log(err); res.send(err.message); }
+        else { res.send(data); }
+      });
+    });
+  })
+  .delete(function(req, res) {
+    var courseID = req.params.id;
+
+    /**/ MongoClient.connect('mongodb://mega-group:398project2@ds053320.mongolab.com:53320/398project2', function handleResponse(err, db) {
+      db.collection('courses').remove({ '_id': courseID  }, function callback(err) {
+        if (err) { console.log(err); res.send(err.message); }
+        else { res.send('success'); }
+      });
+    });
+  })
+  .put(function(req, res) {
+    var courseID = req.params.id;
+
+    /**/ MongoClient.connect('mongodb://mega-group:398project2@ds053320.mongolab.com:53320/398project2', function handleResponse(err, db) {
+      db.collection('courses').save(req.body, function callback(err) {
+        if (err) { console.log(err); res.send(err.message); }
+        else { res.send('success'); }
+      });
+    });
+  });
+
+module.exports = router;
