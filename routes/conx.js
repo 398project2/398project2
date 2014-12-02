07c05b77fcb9cbb5e8aecc28e39bd4545233edff
Bryan Jensen <bryanawjensen@gmail.com>
2014-12-02 14:17:28 -0500

    Restructuring conx to allow for user interactions, moving api to /conx/api/, and adding stuff to gitignore

--- routes/conx.js
+++ routes/conx.js
@@ -2,38 +2,45 @@ var express = require('express'),
     MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
 
-// Middleware, used to load db connection into variable
-router.use(function(req, res, next) {
-  MongoClient.connect('mongodb://mega-group:398project2@ds053320.mongolab.com:53320/398project2', function handleResponse(err, db) {
-    req.dbCollection = db.collection('connections');
-    next();
-  });
+router.get('/', function(req, res) {
+  res.render('index');
 });
 
-/* GET users listing. */
-router.route('/:id').get(function(req, res) {
-  var connectionID = req.params.id;
+router
+  // Middleware to load database for all following method routes
+  .use(function(req, res, next) {
+    MongoClient.connect('mongodb://mega-group:398project2@ds053320.mongolab.com:53320/398project2', function handleResponse(err, db) {
+      req.dbCollection = db.collection('connections');
+      next();
+    });
+  })
+  .route('/api/:id')
+  // GET
+  .get(function(req, res) {
+    var connectionID = req.params.id;
 
-  req.dbCollection.find({ '_id': connectionID  }).toArray(function callback(err, data) {
-    if (err) { console.log(err); res.send(err.message); }
-    else { res.send(data); }
-  });
-})
-.delete(function(req, res) {
-  var connectionID = req.params.id;
+    req.dbCollection.find({ '_id': connectionID  }).toArray(function callback(err, data) {
+      if (err) { console.log(err); res.send(err.message); }
+      else { res.send(data); }
+    });
+  })
+  // DELETE
+  .delete(function(req, res) {
+    var connectionID = req.params.id;
 
-  req.dbCollection.remove({ '_id': connectionID  }, function callback(err) {
-    if (err) { console.log(err); res.send(err.message); }
-    else { res.send('success'); }
-  });
-})
-.put(function(req, res) {
-  var connectionID = req.params.id;
+    req.dbCollection.remove({ '_id': connectionID  }, function callback(err) {
+      if (err) { console.log(err); res.send(err.message); }
+      else { res.send('success'); }
+    });
+  })
+  // PUT
+  .put(function(req, res) {
+    var connectionID = req.params.id;
 
-  req.dbCollection.save(req.body, function callback(err) {
-    if (err) { console.log(err); res.send(err.message); }
-    else { res.send('success'); }
+    req.dbCollection.save(req.body, function callback(err) {
+      if (err) { console.log(err); res.send(err.message); }
+      else { res.send('success'); }
+    });
   });
-});
 
 module.exports = router;
