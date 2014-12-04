868d1c90510e5e743cc61a43d22d8b39bc156b35
tkicks <tkicks32@gmail.com>
2014-12-03 19:21:06 -0500

    minus CONX saving

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require('express'),
+	MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+
+  var userID = req.params.id;
+
+    MongoClient.connect('mongodb://mega-group:398project2@ds053320.mongolab.com:53320/398project2', function handleResponse(err, db) {
+      res.send('connected');
+      db.collection('user-info').find(/*computer id*/req.body, function callback(err) {
+        if (err) { console.log(err); res.send(err.message); }
+        else { res.send('success'); }
+      });
+    });
+})
+.put(function(req, res) {
+    var userID = req.params.id;
+
+    MongoClient.connect('mongodb://mega-group:398project2@ds053320.mongolab.com:53320/398project2', function handleResponse(err, db) {
+      db.collection('user-info').save(/*computer id*/req.body, function callback(err) {
+        if (err) { console.log(err); res.send(err.message); }
+        else { res.send('success'); }
+      });
+    });
 });
 
 module.exports = router;
