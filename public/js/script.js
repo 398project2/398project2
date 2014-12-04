e44da7de078e6e3e2c028fd64aaca06c3f2b423e
Bryan Jensen <bryanawjensen@gmail.com>
2014-12-03 19:01:45 -0500

    Got the last added input to be focused

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,11 @@
 $(function() {
     $('#add-course').click(function() {
         var lastCourse = $(this).parent().siblings('.course').last();
-        lastCourse.clone().find('input').attr('name', parseInt(lastCourse.find('input').attr('name')) + 1).parent().insertAfter(lastCourse);
+        var lastName = parseInt(lastCourse.find('input').attr('name'));
+        var clone = lastCourse.clone();
+        clone.insertAfter(lastCourse)
+            .find('input').attr('name', lastName + 1)
+            .focus();
+        // console.log(clone);
     });
 });
